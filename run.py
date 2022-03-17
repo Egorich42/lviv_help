d40578d0ab4c06664e578840671dfb3f3d607cf0
egorich42 <y.chuchvaha@quantumobile.com>
2022-03-17 11:46:46 +0200

    added timestamp to requests and modified docker-struct

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from lviv_help.views import app
 
 if __name__ == "__main__":
 	app.config['SECRET_KEY'] = "Your_secret_string"
-	# app.run(debug=True, host='0.0.0.0', port=4998)
-	app.run()
+	app.run(debug=True, host='0.0.0.0', port=4998)
+	#app.run()
